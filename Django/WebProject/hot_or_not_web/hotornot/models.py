66f7e85ddc219e5b550491f9cc145cc1e3d01160
levvism <levvis.m@googlemail.com>
2014-01-25 14:05:17 +0000

    added models for database

--- Django/WebProject/hot_or_not_web/hotornot/models.py
+++ Django/WebProject/hot_or_not_web/hotornot/models.py
@@ -1,3 +1,121 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Timestamp(models.Model):
+    tid = models.IntegerField(primary_key=True, db_column='TID') # Field name made lowercase.
+    vid = models.IntegerField(db_column='vID') # Field name made lowercase.
+    time = models.TextField(db_column='Time') # Field name made lowercase. This field type is a guess.
+    date = models.DateField(db_column='Date') # Field name made lowercase.
+    day = models.IntegerField(db_column='Day') # Field name made lowercase.
+    class Meta:
+        db_table = 'Timestamp'
+
+class Venue(models.Model):
+    vid = models.CharField(max_length=24L, primary_key=True, db_column='vID') # Field name made lowercase.
+    name = models.TextField(db_column='Name') # Field name made lowercase.
+    type = models.TextField(db_column='Type') # Field name made lowercase.
+    latitude = models.FloatField(db_column='Latitude') # Field name made lowercase.
+    longitude = models.FloatField(db_column='Longitude') # Field name made lowercase.
+    class Meta:
+        db_table = 'Venue'
+
+class Venuedata(models.Model):
+    vid = models.IntegerField(primary_key=True, db_column='vID') # Field name made lowercase.
+    name = models.TextField(db_column='Name') # Field name made lowercase.
+    hits = models.IntegerField(db_column='Hits') # Field name made lowercase.
+    class Meta:
+        db_table = 'VenueData'
+
+class AuthGroup(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=80L, unique=True)
+    class Meta:
+        db_table = 'auth_group'
+
+class AuthGroupPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)
+    group_id = models.IntegerField()
+    permission_id = models.IntegerField()
+    class Meta:
+        db_table = 'auth_group_permissions'
+
+class AuthPermission(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50L)
+    content_type_id = models.IntegerField()
+    codename = models.CharField(max_length=100L)
+    class Meta:
+        db_table = 'auth_permission'
+
+class AuthUser(models.Model):
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=128L)
+    last_login = models.DateTimeField()
+    is_superuser = models.IntegerField()
+    username = models.CharField(max_length=30L, unique=True)
+    first_name = models.CharField(max_length=30L)
+    last_name = models.CharField(max_length=30L)
+    email = models.CharField(max_length=75L)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+    class Meta:
+        db_table = 'auth_user'
+
+class AuthUserGroups(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField()
+    group_id = models.IntegerField()
+    class Meta:
+        db_table = 'auth_user_groups'
+
+class AuthUserUserPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField()
+    permission_id = models.IntegerField()
+    class Meta:
+        db_table = 'auth_user_user_permissions'
+
+class DjangoAdminLog(models.Model):
+    id = models.IntegerField(primary_key=True)
+    action_time = models.DateTimeField()
+    user_id = models.IntegerField()
+    content_type_id = models.IntegerField(null=True, blank=True)
+    object_id = models.TextField(blank=True)
+    object_repr = models.CharField(max_length=200L)
+    action_flag = models.IntegerField()
+    change_message = models.TextField()
+    class Meta:
+        db_table = 'django_admin_log'
+
+class DjangoContentType(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100L)
+    app_label = models.CharField(max_length=100L)
+    model = models.CharField(max_length=100L)
+    class Meta:
+        db_table = 'django_content_type'
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(max_length=40L, primary_key=True)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+    class Meta:
+        db_table = 'django_session'
+
+class DjangoSite(models.Model):
+    id = models.IntegerField(primary_key=True)
+    domain = models.CharField(max_length=100L)
+    name = models.CharField(max_length=50L)
+    class Meta:
+        db_table = 'django_site'
+
