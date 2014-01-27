9d3a59353ded5711444f832aa0c20634df5520cd
Anton Belev <belevanton@gmail.com>
2014-01-27 22:49:52 +0000

    added new jar - udf is now without constructor

--- DataMining/Pig/UDF/MyUDFs/src/com/anton/hadoop/pig/production/GenerateVenueUDF.java
+++ DataMining/Pig/UDF/MyUDFs/src/com/anton/hadoop/pig/production/GenerateVenueUDF.java
@@ -1,33 +1,34 @@
 package com.anton.hadoop.pig.production;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.Tuple;
-import org.apache.pig.impl.util.UDFContext;
 
 public class GenerateVenueUDF extends EvalFunc<String> {
 	private String regex;
 	private static Pattern p;
 	
-	public GenerateVenueUDF(String fileName) throws IOException{	
-		FileSystem fs = FileSystem.get(UDFContext.getUDFContext().getJobConf());
-		FSDataInputStream in = fs.open(new Path(fileName));
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		regex = br.readLine(); // should be one line only !!!
-		p = Pattern.compile(regex);
-	}
+//	public GenerateVenueUDF() throws IOException{
+//		String fileName = "venues_regex.txt";	
+//		FileSystem fs = FileSystem.get(UDFContext.getUDFContext().getJobConf());
+//		FSDataInputStream in = fs.open(new Path(fileName));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+//		regex = br.readLine(); // should be one line only !!!
+//		p = Pattern.compile(regex);
+//	}
 
 	@Override
 	public String exec(Tuple tuple) throws IOException {
 		// expect one string
+		Scanner sn = new Scanner(new File("/users/level3/1103816b/Desktop/HotOrNot_repo/DataMining/Pig/venues_regex.txt"));
+		regex = sn.nextLine(); // should be one line only !!
+		sn.close();
+		p = Pattern.compile(regex);
 		
 		if (tuple == null) {
 			throw new IllegalArgumentException(
