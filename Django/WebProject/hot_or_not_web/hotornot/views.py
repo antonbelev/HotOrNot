7c9c6a53684e5b1aef0bc80133eeb512ffd8d6f8
Anton Belev <belevanton@gmail.com>
2014-02-14 16:04:18 +0000

    json for the charts is now returned by the view

--- Django/WebProject/hot_or_not_web/hotornot/views.py
+++ Django/WebProject/hot_or_not_web/hotornot/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
-
-from django.http import HttpResponse
+import json
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -11,7 +10,13 @@ def base(request):
     #read venues from db here
     venues = ['o2 Academy', 'Firhill Stadium', 'University of Glasgow']
     
-    context_dict['venues'] = venues    
+    jsonData = { "2009" : {"o2 Academy" : 2534, "University of Glasgow" : 3244, "Firhill Stadium" : 3425},
+    "2010" : {"o2 Academy" : 4524, "University of Glasgow" : 4566, "Firhill Stadium" : 7674},
+    "2011" : {"o2 Academy" : 1234, "University of Glasgow" : 4534, "Firhill Stadium" : 6343}
+    }
+    
+    context_dict['venues'] = venues  
+    context_dict['json'] = json.dumps(jsonData)     
     
     return render_to_response('home/statistics.html', context_dict, context)
 
--- Django/WebProject/hot_or_not_web/static/js/data.js
+++
@@ -1,7 +0,0 @@
-
-
- var json = { "2009" : {"o2 Academy" : 2534, "University of Glasgow" : 3244, "Firhill Stadium" : 3425},
-	"2010" : {"o2 Academy" : 4524, "University of Glasgow" : 4566, "Firhill Stadium" : 7674},
-	"2011" : {"o2 Academy" : 1234, "University of Glasgow" : 4534, "Firhill Stadium" : 6343}
- 
- };
